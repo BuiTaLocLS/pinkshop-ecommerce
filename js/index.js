532d10bde844e6b97f718ba5410e7256747b0715
BuiTaLocLS <btloc2304@gmail.com>
2022-07-20 03:31:13 +0700

    make slide animation and js slide

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
 
@@ -16,6 +15,49 @@ function shuffle(array) {
 
     return array;
 }
+
+// Slider
+let slide_index = 0
+let slide_play = true
+let slides = document.querySelectorAll('.slide')
+
+hideAllSlide = () => {
+    slides.forEach(e => {
+        e.classList.remove('active')
+    })
+}
+
+showSlide = () => {
+    hideAllSlide()
+    slides[slide_index].classList.add('active')
+}
+
+nextSlide = () => slide_index = slide_index + 1 === slides.length ? 0 : slide_index + 1
+
+prevSlide = () => slide_index = slide_index - 1 < 0 ? slides.length - 1 : slide_index - 1
+
+// pause slide when hover slider
+
+document.querySelector('.slider').addEventListener('mouseover', () => slide_play = false)
+
+// enable slide when mouse leave out slider
+document.querySelector('.slider').addEventListener('mouseleave', () => slide_play = true)
+
+// slider controll
+
+document.querySelector('.slide-next').addEventListener('click', () => {
+    nextSlide()
+    showSlide()
+})
+
+document.querySelector('.slide-prev').addEventListener('click', () => {
+    prevSlide()
+    showSlide()
+})
+
+showSlide()
+
+// categories
 
 let categories = [
     {
@@ -83,7 +125,7 @@ categories.forEach(e => {
     categories_list.insertAdjacentHTML("beforeend", prod)
 })
 
-
+// new products
 let new_products = [
     {
         category: 'JACKET',
